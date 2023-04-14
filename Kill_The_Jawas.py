81aaa7aaa89ad677189fa9f3d87b56e473e62ce6
24JMorcom <24JMorcom@susq.k12.pa.us>
2023-04-14 09:40:27 -0400

    useful name

--- Kill_The_Jawas.py
+++ Kill_The_Jawas.py
@@ -1,8 +1,8 @@
 #Inventory
 #armor stats [name,mgk_def,atk_def]
-armor_bank=[["Leather Armor"]]
-weapons_bank=[]
-potions_bank=[]
-misc_bank=[]
-
+#weapon stats [name,atk_dmg,mgk_dmg]
+#potion stats [name,dmg,health]
+armor_bank=[["Leather Armor",2,2],["Studded Leather Armor",3,3],["Chainmail Armor",5,5],["Iron Armor",7,7],["Steel Armor",9,9],["Wizard Cloak",2,13],["Mithril",10,14]]
+weapons_bank=[["Stick",1,0],["Wooden Sword",2,0],["Plastic Lightsaber",1,12],["Morning Star",4,0],["Blessed Chancla",5,5],["Crossbow",8,0],["Inferno Staff",1,15],["Mithril Longsword",20,22],["Iron Shortsword",7,0],["Holy Warhammer",17,5]]
+potions_bank=[["Healht Potion I",0,4],["Health Potion II",0,6],["Health Potion III",0,8],["Poison Poison I",4,0],["Posion Potion II",6,0],["Poison Potion III",8,0]]
 
